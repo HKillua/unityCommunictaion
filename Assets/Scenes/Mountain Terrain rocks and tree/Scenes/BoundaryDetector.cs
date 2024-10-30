f65aaf15e0cee1420281a7f082422fa79dacfe4c
naboyuan <boyuankira@gmail.com>
2024-10-30 10:30:13 +0800

    add newUIscenes

--- Assets/Scenes/Mountain Terrain rocks and tree/Scenes/BoundaryDetector.cs
+++ Assets/Scenes/Mountain Terrain rocks and tree/Scenes/BoundaryDetector.cs
@@ -4,33 +4,7 @@ using UnityEngine;
 
 public class BoundaryDetector : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("CombatUnit"))
-        {
-            Debug.Log("Combat unit entered the boundary.");
-            CombatUnit combatUnit = other.GetComponent<CombatUnit>();
-            if (combatUnit != null)
-            {
-                combatUnit.SetOutOfBoundary(false); // 进入边界时设置标志位
-            }
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("CombatUnit"))
-        {
-            Debug.Log("Combat unit exited the boundary.");
-            CombatUnit combatUnit = other.GetComponent<CombatUnit>();
-            if (combatUnit != null)
-            {
-                Vector3 boundaryCenter = transform.position;
-                combatUnit.SetOutOfBoundary(true); // 离开边界时设置标志位
-                combatUnit.ReturnToBoundary(boundaryCenter); // 立刻返回
-            }
-        }
-    }
+   
 }
 
 
--- Assets/Scenes/menu/HillJump.cs
+++ Assets/Scenes/menu/HillJump.cs
@@ -6,18 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class HillJump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+  
     public void HillJumpScene()
     {
         SceneManager.LoadScene(1);
